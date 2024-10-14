7317f4e45d79ff781bba0054e0a0d4bb66f37fd3
milk_wang <1248160312@qq.com>
2024-10-14 11:22:13 +0800

    feat: aa

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,18 +1,23 @@
-import * as path from 'path';
-import { defineConfig } from 'rspress/config';
+import * as path from "path";
+import { defineConfig } from "rspress/config";
 
 export default defineConfig({
-  root: path.join(__dirname, 'docs'),
-  title: 'Rspress',
-  description: 'Rspack-based Static Site Generator',
-  icon: '/rspress-icon.png',
+  base: "/rspress-demo",
+  root: path.join(__dirname, "docs"),
+  title: "Rspress",
+  description: "Rspack-based Static Site Generator",
+  icon: "/rspress-icon.png",
   logo: {
-    light: '/rspress-light-logo.png',
-    dark: '/rspress-dark-logo.png',
+    light: "/rspress-light-logo.png",
+    dark: "/rspress-dark-logo.png",
   },
   themeConfig: {
     socialLinks: [
-      { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
+      {
+        icon: "github",
+        mode: "link",
+        content: "https://github.com/web-infra-dev/rspress",
+      },
     ],
   },
 });
